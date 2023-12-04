c48a98539dd7f825852d7a818d1644218cbaa140
takuma432 <takuma432@s322.xrea.com>
2023-12-04 17:25:52 +0900

    cors.phpでbuild時に環境変数がnullになる問題の対策

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,8 @@
 return [
 
   'paths' => ['*'],
-  'allowed_methods' => ['*'],
-  'allowed_origins' => [env('SESSION_PROTOCOL') . env('SANCTUM_STATEFUL_DOMAINS')],
+  'allowed_methods' => ['*'], //buildで環境変数が読み込まれない、キャッシュクリアしてもだめ、一時しのぎで本番用のデフォルト値を設定しておく
+  'allowed_origins' => [env('SESSION_PROTOCOL') . env('SANCTUM_STATEFUL_DOMAINS') || 'https://takuma432.shop'],
   'allowed_origins_patterns' => [],
   'allowed_headers' => ['*'],
   'exposed_headers' => ['*'],
